843f779a07954bf4e5c0debc589936b787ef920a
sunweisheng <sunweisheng@live.cn>
2025-06-09 22:40:02 +0800

    number_exercise

---
+++ number_exercise.py
@@ -0,0 +1,47 @@
+print(2+3)
+
+print(3-2)
+
+print(2*3)
+
+print(3/2)
+
+# 乘方运算
+print(3 ** 2)
+
+print(3 ** 3)
+
+print (10 ** 6)
+
+print (2+3*4)
+print ((2+3)*4)
+
+# 浮点数(运算中有一个浮点数答案就是浮点数，除法结果都是浮点数)
+
+print(0.1+0.1)
+
+print(0.2+0.2)
+
+print(2*0.1)
+
+print(0.2+0.1)
+
+print(3*0.1)
+
+print(4/2)
+
+print(1+2.0)
+
+print(2*3.0)
+
+print(3.0 ** 2)
+
+# 数字很大时用_分组，随意分组没有固定位数
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+# 同时给多个变量赋值
+x,y,z = 0, 1, 2
+
+print(x,y,z)
